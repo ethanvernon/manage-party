3d05fcf43cb35305588be7bcf454f9a61681cdad
Ethan Vernon <ethan.vernon@colorado.edu>
2018-12-10 12:35:19 +0800

    Updates

---
+++ precache-manifest.2ac0c09644fea85f85d376edb3915f68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/party-manager/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b6aff04aeb7e447e77b",
+    "url": "/party-manager/static/js/runtime~main.0b6aff04.js"
+  },
+  {
+    "revision": "b1225aef38e0905dcc93",
+    "url": "/party-manager/static/js/main.b1225aef.chunk.js"
+  },
+  {
+    "revision": "6879bbf3104d8d374383",
+    "url": "/party-manager/static/js/1.6879bbf3.chunk.js"
+  },
+  {
+    "revision": "b1225aef38e0905dcc93",
+    "url": "/party-manager/static/css/main.d6b93033.chunk.css"
+  },
+  {
+    "revision": "6879bbf3104d8d374383",
+    "url": "/party-manager/static/css/1.98953f53.chunk.css"
+  },
+  {
+    "revision": "e6eb49a1d1d2018045797edc8ba7205b",
+    "url": "/party-manager/index.html"
+  }
+];
