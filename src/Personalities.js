5b58b1a8fcd6c34d0b8a77742e355bb575f72941
Ethan Vernon <ethan.vernon@colorado.edu>
2018-12-13 19:39:54 +0800

    Removed unused applyCurrentClass function from Personalities.js

--- src/Personalities.js
+++ src/Personalities.js
@@ -10,33 +10,13 @@ import {CharacterNavBar} from './CharacterNavBar';
 export class Personalities extends Component {
 	constructor(props) {
 		super(props);
-		this.handleClick = this.handleClick.bind(this);
 	}
 
-	//passes argument back to updateCurrentPersonality in Parent.js
-	handleClick(name) {
-		console.log(name.currentTarget.innerText);
-		this.props.onClick(name.currentTarget.innerText);
-
-		this.applyCurrentClass(name);
-	}
-
-	//applies css class "current" to current viewed tab
-	applyCurrentClass(name) {
-		var elements = document.getElementsByClassName("person");
-
-		for (var i=0; i<elements.length; i++) {
-			elements[i].classList.remove("current");
-		}
-
-		name.currentTarget.className = "person current";
-	}
-
-	//render function
+	//renders a CharacterNavBar and the appropriate character info based on this.props.current
+	//this.props.current is equal to Parent.js this.state.currentPersonality
 	render() {
 
 		let page;
-
 
 	    //renders appropriate personality info depending on this.props.current
 	    switch (this.props.current) {
